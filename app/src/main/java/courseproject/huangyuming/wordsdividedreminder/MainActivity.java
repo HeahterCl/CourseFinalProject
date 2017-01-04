bfb7d22ac33a17dafa07186874e6ef4b56b598d4
HuangYuming <623813445@qq.com>
2017-01-05 00:25:53 +0800

    响应式布局的初步实现

--- app/src/main/java/courseproject/huangyuming/wordsdividedreminder/MainActivity.java
+++ app/src/main/java/courseproject/huangyuming/wordsdividedreminder/MainActivity.java
@@ -15,6 +15,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
@@ -110,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(alarmReceiver, intentfileter);
 
         setupListRecyclerView();
+
     }
 
     // 将备忘事项按时间分组
