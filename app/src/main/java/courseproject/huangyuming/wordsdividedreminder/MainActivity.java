7e966d8310cecc648af65e1c5cd64f5ce41d8829
HuangYuming <623813445@qq.com>
2017-01-07 08:05:26 +0800

    搜索页面

--- app/src/main/java/courseproject/huangyuming/wordsdividedreminder/MainActivity.java
+++ app/src/main/java/courseproject/huangyuming/wordsdividedreminder/MainActivity.java
@@ -329,7 +329,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
+            startActivity(new Intent(MainActivity.this, SearchActivity.class));
             return true;
         }
 
